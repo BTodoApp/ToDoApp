4f87765e1d6f8d50b4da0804c9eece3e598e44df
Brynner03 <bventura03@hotmail.com>
2023-05-25 16:43:10 -0400

    Changed alt title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="title">Welcome to ToDoApp</div>
       <div className="content">
         <div className="image-container">
-          <img src={image} alt="Picture of application" className="image" />
+          <img src={image} alt="Boards Display" className="image" />
         </div>
       </div>
     </div>
