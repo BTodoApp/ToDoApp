a8f1b82915b65faf746e6fd30c8dbac1d44794c8
Brynner03 <bventura03@hotmail.com>
2023-05-25 16:24:18 -0400

    Updated image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import image from '../images/image.png';
 import "../styles/Home.css"
 
 const Home = () => {
@@ -8,7 +7,7 @@ const Home = () => {
       <div className="title">Welcome to ToDoApp</div>
       <div className="content">
         <div className="image-container">
-          <img src={image} alt="Picture of application" className="image" />
+          <img src="../images/image.png" alt="Picture of application" className="image" />
         </div>
       </div>
     </div>
